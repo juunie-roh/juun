2229b1b14d7e882b411b6fb7325a595cf3199c79
Juun <blacklily7513@gmail.com>
2024-11-25 17:32:36 +0900

    feat(Modal): added new modal component

---
+++ src/components/shared/Modal.stories.tsx
@@ -0,0 +1,78 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { useRef } from 'react';
+
+import type { ModalRef } from '@/types/ui.types';
+
+import Button from './Button';
+import Modal from './Modal';
+
+const meta: Meta<typeof Modal> = {
+  title: 'Components/Actions/Modal',
+  component: Modal,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    title: 'Modal Title',
+    content: 'Modal Content',
+    interactive: undefined,
+    onClose: fn(),
+  },
+  argTypes: {
+    title: {
+      control: 'object',
+      description: 'a title of modal',
+      table: {
+        defaultValue: { summary: 'undefined' },
+        type: {
+          summary: 'React.ReactNode | string | undefined',
+        },
+      },
+    },
+    content: {
+      control: 'object',
+      description: 'content of modal',
+      table: {
+        defaultValue: { summary: 'undefined' },
+        type: {
+          summary: 'React.ReactNode | string | undefined',
+        },
+      },
+    },
+    footer: {
+      control: 'object',
+      description: 'footer of modal',
+      table: {
+        defaultValue: { summary: 'undefined' },
+        type: {
+          summary: 'React.ReactNode | string | undefined',
+        },
+      },
+    },
+    interactive: {
+      control: 'boolean',
+      description: 'make modal interactive with ',
+    },
+  },
+  render: (args) => {
+    const modal = useRef<ModalRef>(null);
+    return (
+      <>
+        <Button variant="neutral" onClick={() => modal.current?.open()}>
+          open
+        </Button>
+        <Modal ref={modal} {...args} />
+      </>
+    );
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Modal>;
+
+// Base story
+export const Default: Story = {
+  args: {},
+};
---
+++ src/components/shared/Modal.tsx
@@ -0,0 +1,81 @@
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+
+import type { ModalProps, ModalRef } from '@/types/ui.types';
+
+const Modal = forwardRef<ModalRef, ModalProps>((props, ref) => {
+  const {
+    title,
+    content,
+    footer,
+    interactive = false,
+    onClose,
+    className = '',
+  } = props;
+
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    open: () => {
+      dialogRef.current?.showModal();
+    },
+    close: () => {
+      dialogRef.current?.close();
+      onClose?.();
+    },
+    toggle: () => {
+      if (dialogRef.current?.open) {
+        dialogRef.current.close();
+        onClose?.();
+      } else {
+        dialogRef.current?.showModal();
+      }
+    },
+  }));
+
+  return (
+    <dialog ref={dialogRef} className={`modal ${className}`}>
+      <div className="modal-box">
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+        {/* Title Section */}
+        {title &&
+          (typeof title === 'string' ? (
+            <h3 className="text-lg font-bold">{title}</h3>
+          ) : (
+            title
+          ))}
+
+        {/* Content Section */}
+        {content &&
+          (typeof content === 'string' ? (
+            <p className="py-4">{content}</p>
+          ) : (
+            content
+          ))}
+
+        {/* Footer Section */}
+        <div className="modal-action">
+          {footer || (
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          )}
+        </div>
+      </div>
+
+      {!interactive && (
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      )}
+    </dialog>
+  );
+});
+
+Modal.displayName = 'Modal';
+
+export default Modal;
