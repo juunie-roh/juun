54964c2c5760098cb7eb30755b4067fda9fe5d1e
Juun <blacklily7513@gmail.com>
2024-12-09 12:46:10 +0900

    chore(svg): added assets

--- src/components/features/BearCounter.tsx
+++ src/components/features/BearCounter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { SvgMinus, SvgPlus } from '@/assets/svgs';
+import { Minus, Plus } from '@/assets/svgs';
 import Button from '@/components/shared/action/Button';
 import { useBearStore } from '@/stores/slices/bear';
 
@@ -10,13 +10,13 @@ const BearCounter = () => {
   return (
     <div className="flex items-center justify-center">
       <Button shape="circle" aria-label="increase" onClick={() => increase(1)}>
-        <SvgPlus />
+        <Plus />
       </Button>
       <Button variant="ghost" shape="wide">
         {bears}
       </Button>
       <Button shape="circle" aria-label="decrease" onClick={() => decrease(1)}>
-        <SvgMinus />
+        <Minus />
       </Button>
     </div>
   );
