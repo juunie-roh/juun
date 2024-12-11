3f3084d58649ecf25540e647f659647783aa9051
Juun <blacklily7513@gmail.com>
2024-12-11 13:37:19 +0900

    fix(Badge): added new shared component

---
+++ src/components/shared/datadisplay/Badge.tsx
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+
+import type { BadgeProps } from '@/types/ui.types';
+
+const Badge = (props: BadgeProps) => {
+  const { variant, outline, size = 'md', className, children } = props;
+
+  const badgeClasses = clsx([
+    'badge',
+    // Badge Styles
+    {
+      'badge-neutral': variant === 'neutral',
+      'badge-primary': variant === 'primary',
+      'badge-secondary': variant === 'secondary',
+      'badge-accent': variant === 'accent',
+      'badge-info': variant === 'info',
+      'badge-success': variant === 'success',
+      'badge-warning': variant === 'warning',
+      'badge-error': variant === 'error',
+      'badge-ghost': variant === 'ghost',
+      'badge-outline': outline,
+    },
+    // Badge Sizes
+    {
+      'badge-xs': size === 'xs',
+      'badge-sm': size === 'sm',
+      'badge-md': size === 'md',
+      'badge-lg': size === 'lg',
+    },
+    // Other Tailwindcss Classes
+    className,
+  ]);
+
+  return <div className={badgeClasses}>{children}</div>;
+};
+
+export default Badge;
