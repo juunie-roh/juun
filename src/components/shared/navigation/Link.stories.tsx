d06937e7a697757760943059d6299029ea40a40e
Juun <65842841+juunie-roh@users.noreply.github.com>
2024-12-19 13:49:06 +0900

    Multiple Commits (#25)
    * chore(component): relocated the component
    
    * Link
    relocated from datadisplay to navigation
    
    * build(package): packages updated
    
    * Added new packages for commitlint
    1. conventional-changelog-atom for parser
    2. @commitlint/format for formatter
    3. @commitlint/types for typescript
    
    * Updated existing packages
    - daisyui (4.12.22)
    - next (15.1.1)
    - @commitlint/cli (19.6.1)
    - @commitlint/cz-commitlint (19.6.1)
    - @types/node (22.10.2)
    - @types/react (19.0.2)
    - @types/react-dom (19.0.2)
    - eslint-config-next (15.1.1)
    - tailwindcss (3.4.17)
    
    * ci(config): updated commitlint configuration
    
    * `commitlint.config.ts`
    - Added `parserPreset`
    - Added `formatter`
    - New Rule for 'scope-enum'
    - Specified `prompt.questions.scope`

--- src/components/shared/navigation/Link.stories.tsx
+++ src/components/shared/navigation/Link.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Link from './Link';
 
 const meta: Meta<typeof Link> = {
-  title: 'Components/Data Display/Link',
+  title: 'Components/Navigation/Link',
   component: Link,
   parameters: {
     layout: 'centered',
