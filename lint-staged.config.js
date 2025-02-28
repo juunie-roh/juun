dec1e7bc387fb7359394361154aedfcd26853741
Juun <blacklily7513@gmail.com>
2025-02-28 10:23:04 +0900

    ci(package): lint-staged applied, removed lint-staged script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   '*.+(ts|tsx)': [() => 'yarn tsc -p tsconfig.json --noEmit'],
-  '**/*.+(ts|tsx|js|jsx)': ['eslint --fix --cache', 'prettier --write'],
+  '**/*.+(ts|tsx|js|jsx)': ['eslint --fix'],
 };
