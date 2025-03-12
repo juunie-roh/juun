c547c098c4df03140e0e6c49a63338477036d783
Juun <blacklily7513@gmail.com>
2025-03-12 12:29:07 +0900

    feat(component): Add three cannon raycast vehicle example
    * Add packages:
    three, @react-three/cannon, @react-three/drei, @react-three/fiber, @types/three
    
    * Add public assest: models

---
+++ apps/web/components/three/plane.tsx
@@ -0,0 +1,20 @@
+import type { PlaneProps } from '@react-three/cannon';
+import { usePlane } from '@react-three/cannon';
+import type { Ref } from 'react';
+import type { Group } from 'three';
+
+export function Plane(props: PlaneProps) {
+  const [ref] = usePlane(() => ({
+    material: 'ground',
+    type: 'Static',
+    ...props,
+  }));
+  return (
+    <group ref={ref as Ref<Group>}>
+      <mesh receiveShadow castShadow>
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial color="#303030" />
+      </mesh>
+    </group>
+  );
+}
