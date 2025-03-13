27f45e90c3dc62ddc1e6ee1bf895452117e1d71e
Juun <blacklily7513@gmail.com>
2025-03-13 09:41:48 +0900

    chore(asset): Add fonts and images

--- apps/web/assets/fonts/index.ts
+++ apps/web/assets/fonts/index.ts
@@ -17,3 +17,21 @@ export const antonio = localFont({
   variable: '--font-antonio',
   weight: '100 400 700',
 });
+
+export const rixBold = localFont({
+  src: 'RixB.ttf',
+  variable: '--font-rix-bold',
+  weight: '700',
+});
+
+export const rixMedium = localFont({
+  src: 'RixM.ttf',
+  variable: '--font-rix-medium',
+  weight: '500',
+});
+
+export const rixLight = localFont({
+  src: 'RixL.ttf',
+  variable: '--font-rix-light',
+  weight: '300',
+});
