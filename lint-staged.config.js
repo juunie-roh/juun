1e94b1a7494aa3d8689f58b79bbc35d86759b321
Juun <blacklily7513@gmail.com>
2025-03-25 09:35:49 +0900

    chore(config): Update tailwind and lint-staged config
    * tailwind: extend font color scheme for typography
    * lint-staged: replace yarn with pnpm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
   'apps/web/**/*.+(ts|tsx)': [
-    () => 'yarn tsc -p apps/web/tsconfig.json --noEmit',
+    () => 'pnpm tsc -p apps/web/tsconfig.json --noEmit',
   ],
   'packages/ui/**/*.+(ts|tsx)': [
-    () => 'yarn tsc -p packages/ui/tsconfig.json --noEmit',
+    () => 'pnpm tsc -p packages/ui/tsconfig.json --noEmit',
   ],
   '**/*.+(ts|tsx|js|jsx)': ['eslint --fix --cache'],
   '**/*.+(yml|yaml)': ['yamllint .'],
