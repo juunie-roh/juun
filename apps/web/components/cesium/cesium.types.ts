36d530fccc36fb98b8a6e13bc49a60750ce7c1b9
Juun <blacklily7513@gmail.com>
2025-05-15 09:49:11 +0900

    docs: Add default value description to the viewer props

--- apps/web/components/cesium/cesium.types.ts
+++ apps/web/components/cesium/cesium.types.ts
@@ -2,13 +2,13 @@ import { Viewer } from 'cesium';
 import { ViewerProps } from 'resium';
 
 interface IViewerProps extends ViewerProps {
-  /** Whether to show the credit container */
+  /** Whether to show the credit container. @default true */
   bottomContainer?: boolean;
   /** Initial location of the camera */
   flyTo?: Parameters<Viewer['camera']['flyTo']>[0];
-  /** Whether to show the timeline CesiumWidget */
+  /** Whether to show the timeline CesiumWidget. @default true */
   timeline?: boolean;
-  /** Whether to show the animation CesiumWidget */
+  /** Whether to show the animation CesiumWidget. @default true */
   animation?: boolean;
 }
 
