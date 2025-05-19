a8ce257437adfeb3d27beff34a6b617b0705c004
Juun <blacklily7513@gmail.com>
2025-05-19 17:39:30 +0900

    chore: Set initial camera position for terrain demo

--- apps/web/constants/cesium.constants.tsx
+++ apps/web/constants/cesium.constants.tsx
@@ -22,6 +22,18 @@ export const API_FEATURE_OPTIONS: ApiFeatureOption[] = [
   {
     feat: 'terrain',
     label: 'Terrain',
+    flyTo: {
+      destination: new Cartesian3(
+        -3046596.558550092,
+        4065701.630895504,
+        3854536.407434127,
+      ),
+      orientation: {
+        heading: CMath.toRadians(0),
+        pitch: CMath.toRadians(-45),
+        roll: 0.0,
+      },
+    },
   },
   {
     feat: 'viewer',
