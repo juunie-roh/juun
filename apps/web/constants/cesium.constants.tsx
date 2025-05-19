bc983f9417a4e3ae9bae00415165ab7ad301788b
Juun <blacklily7513@gmail.com>
2025-05-19 16:03:36 +0900

    refactor: Set the initial camera position optionally

--- apps/web/constants/cesium.constants.tsx
+++ apps/web/constants/cesium.constants.tsx
@@ -1,9 +1,23 @@
+import { Cartesian3, Math as CMath } from 'cesium';
+
 import { ApiFeatureOption } from '@/types/cesium.types';
 
 export const API_FEATURE_OPTIONS: ApiFeatureOption[] = [
   {
     feat: 'collection',
     label: 'Collection',
+    flyTo: {
+      destination: new Cartesian3(
+        -3964624.632297504,
+        3356819.574895879,
+        3696707.310427818,
+      ),
+      orientation: {
+        heading: CMath.toRadians(0),
+        pitch: CMath.toRadians(-50),
+        roll: 0.0,
+      },
+    },
   },
   {
     feat: 'terrain',
