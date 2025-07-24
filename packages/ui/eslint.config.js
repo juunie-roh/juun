4ddea3fd1d24ccbaa45920bcde71a8eb75cbda3e
Juun <blacklily7513@gmail.com>
2025-07-24 12:31:43 +0900

    fix: Remove unused configs

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,21 +1,13 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
 
-import {
-  a11y,
-  base,
-  react,
-  typescript,
-  unitTest,
-  prettier,
-} from "@config/eslint";
+import { a11y, base, react, typescript, prettier } from "@config/eslint";
 
 export default [
   ...base,
   ...a11y,
   ...react,
   ...typescript,
-  ...unitTest,
   ...prettier,
   ...storybook.configs["flat/recommended"],
 ];
