af3da12973edc17e1bb83532aa3199ee0ee5ecbb
Juun <blacklily7513@gmail.com>
2025-07-29 11:17:40 +0900

    revert: Revert lint-staged settings

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,9 +3,6 @@ export default {
   "apps/web/**/*.+(ts|tsx)": [
     () => "pnpm tsc -p apps/web/tsconfig.json --noEmit",
   ],
-  "packages/next-mdx/**/*.+(ts|tsx)": [
-    () => "pnpm tsc -p packages/next-mdx/tsconfig.json --noEmit",
-  ],
   "packages/ui/**/*.+(ts|tsx)": [
     () => "pnpm tsc -p packages/ui/tsconfig.json --noEmit",
   ],
