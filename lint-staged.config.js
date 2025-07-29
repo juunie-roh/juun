6da85143bbad3e5a8d366e1128dcb46b5fde870e
Juun <blacklily7513@gmail.com>
2025-07-29 11:17:12 +0900

    fix: Remove mdx package

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,8 +3,8 @@ export default {
   "apps/web/**/*.+(ts|tsx)": [
     () => "pnpm tsc -p apps/web/tsconfig.json --noEmit",
   ],
-  "apps/cesium-utils/**/*.+(ts|tsx)": [
-    () => "pnpm tsc -p apps/cesium-utils/tsconfig.json --noEmit",
+  "packages/next-mdx/**/*.+(ts|tsx)": [
+    () => "pnpm tsc -p packages/next-mdx/tsconfig.json --noEmit",
   ],
   "packages/ui/**/*.+(ts|tsx)": [
     () => "pnpm tsc -p packages/ui/tsconfig.json --noEmit",
