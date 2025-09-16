a04dad232f42c15ec7c479ed0f08b73ac94aadda
Juun <blacklily7513@gmail.com>
2025-09-16 10:10:10 +0900

    fix: Update grid layout using custom variables

--- apps/web/app/blog/_components/content.tsx
+++ apps/web/app/blog/_components/content.tsx
@@ -10,17 +10,17 @@ export default function BlogContent({
 }) {
   return (
     <section className="bg-background relative">
-      <div className="md:grid-cols-16 m-auto grid grid-cols-12 gap-x-4 px-4 md:gap-x-8">
-        <hr className="mt-18 col-span-full col-start-1 border-t pt-5 md:col-span-8 md:col-start-5 lg:col-span-full" />
+      <div className="grid-cols-responsive gap-x-responsive m-auto grid px-4">
+        <hr className="mt-18 col-span-full col-start-1 border-t pt-5 md:col-span-8 md:col-start-3 lg:col-span-full" />
       </div>
       <div
         id="text-content"
-        className="md:grid-cols-16 relative m-auto grid grid-cols-12 gap-x-4 px-4 md:gap-x-8"
+        className="grid-cols-responsive gap-x-responsive relative m-auto grid px-4"
       >
         <div className="sticky top-24 z-[1] mb-8 hidden h-fit grid-cols-subgrid lg:col-span-4 lg:col-start-13 lg:row-start-1 lg:grid">
           <TableOfContents className="col-span-full" />
         </div>
-        <div className="col-span-full grid w-full grid-cols-subgrid md:col-start-5 lg:row-start-1">
+        <div className="col-span-full grid w-full grid-cols-subgrid md:col-start-3 lg:col-start-5 lg:row-start-1">
           <Prose className="col-span-full mt-8 md:col-span-8">{children}</Prose>
         </div>
       </div>
