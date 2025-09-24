d48118c5ae5debfb4ec58dfe8703576078fb598f
Juun <blacklily7513@gmail.com>
2025-09-24 13:25:51 +0900

    fix: Update image assets

--- apps/web/app/test-md/yarn-berry-demo/page.tsx
+++ apps/web/app/test-md/yarn-berry-demo/page.tsx
@@ -1,9 +1,5 @@
-import { AspectRatio } from "@juun/ui/aspect-ratio";
-import { Skeleton } from "@juun/ui/skeleton";
 import { readFile } from "fs/promises";
-import Image from "next/image";
 import { join } from "path";
-import { Suspense } from "react";
 
 import { BlogHeader } from "@/app/blog/_components/header";
 import { BlogMetadata } from "@/app/blog/_utils/post";
@@ -19,22 +15,6 @@ export default async function YarnBerryMarkdownDemo() {
   return (
     <article className="xl:w-3xl mx-auto w-full max-w-3xl px-2 pb-20 pt-4 md:px-8">
       <BlogHeader metadata={data as BlogMetadata} />
-      {data.image && (
-        <AspectRatio
-          ratio={16 / 9}
-          className="mb-8 size-full rounded-lg bg-[#514c87]"
-        >
-          <Suspense fallback={<Skeleton className="size-full" />}>
-            <Image
-              src={data.image}
-              alt={data.title}
-              className="size-full object-contain p-8"
-              fill
-            />
-          </Suspense>
-        </AspectRatio>
-      )}
-
       <MarkdownRenderer html={html} />
     </article>
   );
