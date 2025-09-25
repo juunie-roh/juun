095a916546a0b49e9afafa9e42e16ce97e543644
Juun <blacklily7513@gmail.com>
2025-09-25 13:59:09 +0900

    feat: Add article carousel in home

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,12 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@juun/ui/carousel";
 import Image from "next/image";
+import { Suspense } from "react";
 
 import { geistMono } from "@/assets/fonts";
 import { BuildingLibrary, CityPlan, InfoCircleOutlined } from "@/assets/svgs";
@@ -8,7 +16,12 @@ import ConfirmWheel from "@/components/wheel/ConfirmWheel";
 import FiveWheel from "@/components/wheel/FiveWheel";
 import FourWheel from "@/components/wheel/FourWheel";
 
+import { BlogCard, BlogCardSkeleton } from "./blog/_components/card";
+import { getPosts } from "./blog/_utils/post";
+
 export default function Home() {
+  const posts = getPosts();
+
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] place-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main
@@ -58,6 +71,27 @@ export default function Home() {
           <ConfirmWheel />
           <FourWheel />
           <FiveWheel />
+        </div>
+        <div className="relative w-full">
+          <h3 className="font-(family-name:--font-stabil-grotesk-trial) mb-4 text-3xl font-bold tracking-tight">
+            Articles
+          </h3>
+          <Carousel opts={{ align: "start" }} className="w-full px-12">
+            <CarouselContent>
+              {posts.map((post) => (
+                <CarouselItem
+                  key={post.slug}
+                  className="basis-[100%] md:basis-1/3"
+                >
+                  <Suspense fallback={<BlogCardSkeleton />} key={post.slug}>
+                    <BlogCard post={post} />
+                  </Suspense>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="-left-0" />
+            <CarouselNext className="-right-0" />
+          </Carousel>
         </div>
       </main>
     </div>
