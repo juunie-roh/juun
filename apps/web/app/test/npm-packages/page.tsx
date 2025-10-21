b86fb502fde7283bcb7ec4e578af6d44cbfd6cd5
Juun <blacklily7513@gmail.com>
2025-10-21 16:36:50 +0900

    refactor: Restructure test page routes & add npm article test

---
+++ apps/web/app/test/npm-packages/page.tsx
@@ -0,0 +1,31 @@
+import post from "@juun/db/post";
+import { notFound } from "next/navigation";
+
+import BlogContent from "@/app/blog/_components/content";
+import BlogFooter from "@/app/blog/_components/footer";
+import BlogHeader from "@/app/blog/_components/header";
+import { BlogMetadata } from "@/app/blog/_utils/post";
+import md from "@/lib/md";
+
+export default async function NpmMdDemo() {
+  const result = await post.get.bySlug("npm-packages");
+  if (result === null) return notFound();
+
+  const metadata: BlogMetadata = {
+    title: result.title,
+    description: result.description,
+    date: result.created_at,
+    tags: result.tags,
+    image: result.image,
+  };
+
+  const parsed = await md.parse(result.content);
+
+  return (
+    <main className="relative">
+      <BlogHeader metadata={metadata} />
+      <BlogContent>{md.render(parsed)}</BlogContent>
+      <BlogFooter metadata={metadata} />
+    </main>
+  );
+}
