73be2e4f0c64a52353bbfae3fb30e541d7b0c3c0
Juun <blacklily7513@gmail.com>
2025-10-21 17:38:11 +0900

    chore: Apply caching to test pages

--- apps/web/app/test/npm-packages/page.tsx
+++ apps/web/app/test/npm-packages/page.tsx
@@ -1,14 +1,14 @@
-import post from "@juun/db/post";
 import { notFound } from "next/navigation";
 
 import BlogContent from "@/app/blog/_components/content";
 import BlogFooter from "@/app/blog/_components/footer";
 import BlogHeader from "@/app/blog/_components/header";
 import { BlogMetadata } from "@/app/blog/_utils/post";
+import cache from "@/lib/cache";
 import md from "@/lib/md";
 
 export default async function NpmMdDemo() {
-  const result = await post.get.bySlug("npm-packages");
+  const result = await cache.post.get.bySlug("npm-packages");
   if (result === null) return notFound();
 
   const metadata: BlogMetadata = {
