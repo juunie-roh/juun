b8ca6bdd18539d5598f0e7d2952a4f423eba517b
Juun <blacklily7513@gmail.com>
2025-10-22 10:13:44 +0900

    build(deps): Update dependencies & remove next lint script

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,11 @@
 import config from "@config/eslint";
+import { defineConfig } from "eslint/config";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default config;
+export default defineConfig([
+  ...config,
+  {
+    // Ignore auto-generated next-intl type definitions
+    ignores: ["messages/ko.d.json.ts"],
+  },
+]);
