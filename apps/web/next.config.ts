0ade946705593f6e4f1dc1b34df45304069ec9da
Juun <blacklily7513@gmail.com>
2025-11-11 10:09:17 +0900

    build(config): Optimize configuration

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,7 +5,9 @@ import webpack from "webpack";
 
 import packageJson from "./package.json" with { type: "json" };
 
+// get cesium version from the project's package.json
 const CESIUM_VERSION = packageJson.dependencies.cesium.replace(/^[\^~]/, "");
+// use Cesium's official CDN URL with specified version dynamically
 const CESIUM_BASE_URL = `https://cdn.jsdelivr.net/npm/cesium@${CESIUM_VERSION}/Build/Cesium/`;
 
 const nextConfig: NextConfig = {
@@ -26,22 +28,14 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "@prisma/engines"],
   experimental: {
     // externalDir: true,
-    // The serverActions value needs to be an object, not a boolean
-    serverActions: {
-      allowedOrigins: ["*"],
-    },
+    // serverActions: {
+    //   allowedOrigins: ["*"],
+    // },
     optimizePackageImports: ["lucide-react"],
   },
-  transpilePackages: [
-    "@juun/ui",
-    "@juun/api",
-    "@juun/db",
-    "@config/eslint",
-    "@config/tailwind",
-    "@config/typescript",
-  ],
+  transpilePackages: ["@juun/ui", "@juun/api", "@juun/db"],
 
-  // webpack configuration
+  // webpack configuration, for explicit webpack build (with --webpack)
   webpack(config, options) {
     // svg loader
     config.module.rules.push({
