3c732277d0696c01575042b8693d74256b0a72e7
Juun <blacklily7513@gmail.com>
2025-11-13 14:57:10 +0900

    refactor: Restore scroll progress bar under blog

--- apps/web/app/blog/[id]/layout.tsx
+++ apps/web/app/blog/[id]/layout.tsx
@@ -1,5 +1,12 @@
+import { ScrollProgressBar } from "@juun/ui/scroll-progress-bar";
+
 export default function BlogItemLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <main className="relative">{children}</main>;
+  return (
+    <main className="relative">
+      <ScrollProgressBar />
+      {children}
+    </main>
+  );
 }
