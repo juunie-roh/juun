7d186e1831414adcbae3bb07195cf6bdb6261123
Juun <blacklily7513@gmail.com>
2025-12-01 10:39:19 +0900

    fix: Disable sorting feature temporarily

--- apps/web/components/timeline/index.tsx
+++ apps/web/components/timeline/index.tsx
@@ -17,19 +17,12 @@ import TimelineTags from "./tag";
 
 interface TimelineProps {
   items: Omit<TimelineItem, "detail">[];
-  order?: "asc" | "desc";
 }
 
-export default function Timeline({ items, order = "desc" }: TimelineProps) {
-  const sortedItems = [...items].sort((a, b) =>
-    order === "desc"
-      ? new Date(b.date).getTime() - new Date(a.date).getTime()
-      : new Date(a.date).getTime() - new Date(b.date).getTime(),
-  );
-
+export default function Timeline({ items }: TimelineProps) {
   return (
     <ol>
-      {sortedItems.map((item) => (
+      {items.map((item) => (
         <li key={`timeline-${item.id}`} className="relative flex h-full gap-0">
           <div className="relative flex w-1/5 min-w-36 shrink-0 flex-col items-end justify-between gap-2 border-r p-2 pl-0 text-muted-foreground">
             <div className="text-right font-victor-serif text-sm text-muted-foreground">
