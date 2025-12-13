0eda1dc2805e2cdb4c46f2b0a550de9fc56ea303
Juun <blacklily7513@gmail.com>
2025-12-13 17:30:25 +0900

    refactor: Fix layouts to match with header update

--- apps/web/layouts/base.tsx
+++ apps/web/layouts/base.tsx
@@ -1,21 +1,9 @@
-import { cn } from "@juun/ui/lib/utils";
 import React from "react";
 
 export default function BaseLayout({
   children,
-  fixHeight,
 }: Readonly<{
   children: React.ReactNode;
-  fixHeight?: boolean;
 }>) {
-  return (
-    <div
-      className={cn(
-        "relative min-h-[calc(100vh-var(--spacing-header)-1px)] px-4 py-8",
-        fixHeight && "h-[calc(100vh-var(--spacing-header)-1px)]",
-      )}
-    >
-      {children}
-    </div>
-  );
+  return <div className="pt-header">{children}</div>;
 }
