2009a725a9b48b8509d2dd275142fa13cc191b40
Juun <blacklily7513@gmail.com>
2026-02-03 14:09:13 +0900

    chore: Fix typo in comment

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -6,7 +6,7 @@ export default createMiddleware(routing);
 
 export const config = {
   matcher: [
-    // Math all pathnames except for
+    // Match all path names except for
     // - ... if they start with `/api`, `/trpc`, `/_next`, `/_vercel`, or `/100days`
     // - ... the ones containing a dot (e.g. `favicon.ico`)
     "/((?!api|trpc|_next|_vercel|100days|.*\\..*).*)",
