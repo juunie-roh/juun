938353cd200929c9371736d2912526278a51ecbb
Juun <blacklily7513@gmail.com>
2026-04-08 10:33:07 +0900

    fix: Remove state handlers on server side page

--- apps/web/app/[locale]/playground/cannon-raycast-vehicle/page.tsx
+++ apps/web/app/[locale]/playground/cannon-raycast-vehicle/page.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-
 import CannonVehicle from "./_components/three/cannon-vehicle";
 
 export default function CannonRaycastVehiclePage() {
-  const [debug, setDebug] = React.useState<boolean>(false);
-
   return (
     <div className="size-full">
-      <CannonVehicle debug={debug} />
+      <CannonVehicle debug={false} />
     </div>
   );
 }
